f55ef0ee9dbdc977ff5f6f31257e265f7c66017f
Kim-JW <bbam03@naver.com>
2022-02-17 16:01:39 +0900

    JS #02

--- javascript-practice/basics/ex02.js
+++ javascript-practice/basics/ex02.js
@@ -9,7 +9,12 @@
  [객체]
  - object type
     1. new 생성자 함수(역할) 사용
-    2. 
+        Number() => object type
+        String() => object type
+        Boolean() => object type
+        Object() => object type
+        Array() => object type
+    2. {} 객체 리터럴, J(ava)S(cript)O(bject)
     3.
     4.
 
@@ -35,12 +40,18 @@ console.log("b: " + typeof(b));
 var a;
 console.log(a);
 
-console.log("====== 객체타입(object type) ======");
+console.log("====== 객체타입1(object type) ======");
 var i2 = new Number(1);
 var s2 = new String("Hello World");
 var b2 = new Boolean(false);
 var o = new Object();
 var a = new Array();        // 배열도 객체다!!
+
+var o2 = {};
+var a2 = [];
+
+var o = new f();
+console.log("o" + typeof(o));
 
 console.log("i2: " + typeof(i2));
 console.log("s2: " + typeof(s2));
@@ -56,4 +67,13 @@ f();
 
 var o = new f();
 
-console.log("====== 객체타입(function type) ======");
+console.log("====== 원시타입과 원시타입의 Wrapper 객체는 구분 없이 사용할 수 있다. ======");
+console.log(i + i2); 
+console.log(s + s2);
+
+// 원시 타입의 메소드가 호출될 때
+// 임시 Wrapper 객체가 만들어져 메소드 호출이 일어난 뒤 사라진다. (유사객체)
+
+console.log("====== 원시타입도 메소드 호출이 가능하다. ======");
+console.log(b.valueOf());
+console.log(b2.valueOf());
