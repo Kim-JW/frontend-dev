1438367a09c92e3ab4deeec497761dff5c9af51d
Kim-JW <bbam03@naver.com>
2022-03-03 15:24:49 +0900

    module standard ex & bad ex

---
+++ javascript-practice/basics/module-ex04/ex01.js
@@ -0,0 +1,7 @@
+var mode01 = require("./error-module01");
+var mode02 = require("./error-module02");
+var mode03 = require("./error-module03");
+
+console.log(mode01);
+mode02();
+console.log(mode03);
