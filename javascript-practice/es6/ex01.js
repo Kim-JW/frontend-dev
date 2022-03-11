3bce4874c1ca5f33c155edbb2ed5cb4f8a097bc8
Kim-JW <bbam03@naver.com>
2022-03-11 09:51:41 +0900

    es6 basic

--- javascript-practice/es6/ex01.js
+++ javascript-practice/es6/ex01.js
@@ -11,5 +11,19 @@ try {
 
     console.log(i, j);
 } catch (e) {
-    console.log('error' + e);
+    console.log('[error]' + e);
 }
+
+// cf. var의 함수 범위
+
+try {
+    let f = function() {
+        var m = 20;
+    }
+    
+    f();    
+    console.log(m);
+} catch (e) {
+    console.log('[error]' + e);    
+}
+
