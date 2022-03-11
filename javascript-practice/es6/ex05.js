774830d40fd5fd348f705e1baac9f8eaa9bfa3cb
Kim-JW <bbam03@naver.com>
2022-03-11 12:05:39 +0900

    function destruct & class Grammer

--- javascript-practice/es6/ex05.js
+++ javascript-practice/es6/ex05.js
@@ -7,4 +7,39 @@ const power = function(x) {
     return x * x;
 }
 
-console.log(power(2));
+const numbers = [1, 2, 3, 4, 5];
+numbers.forEach(function(n){
+    process.stdout.write(`${n}: ${power(n)}\t`);
+});
+
+// ex01
+
+
+// ex02
+console.log("\n ----- \n");
+const power2 = x => x*x;
+
+numbers.forEach(function(n) {
+    process.stdout.write(`${n}: ${(x => x*x)(n)}\t`);
+});
+
+
+// ex03 
+console.log("\n ----- \n");
+
+numbers.forEach(n => process.stdout.write(`${n}: ${(x => x*x)(n)}\t`));
+
+// ex04 : this를 어휘적으로 바인딩 가능하다.
+console.log("\n ----- \n");
+const dooly = {
+    name: '둘리',
+    friends: ['또치', '마이콜', '도우너', '길동'],
+    printFriends: function() {
+        // this.friends.forEach(function(f){
+        //     console.log(`${this.name}의 친구 ${f}`);
+        // }.bind(this));
+        this.friends.forEach(f => console.log(`${this.name}의 친구 ${f}`));
+    }
+};
+
+dooly.printFriends();
